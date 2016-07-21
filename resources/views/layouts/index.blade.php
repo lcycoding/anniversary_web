24c6e6e30a2ca9119df9b0b1029e8cbffcb214eb
LincheYu <lcycoding@gmail.com>
2016-07-21 15:47:14 +0800

    ready to go

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -8,19 +8,20 @@
     To our 4th Anniversary
     </title>
     <style>
+
     body, html {
     height: 100%;
     margin: 0;
     font: 400 25px/1.8 "Lato", sans-serif;
     color: #777;
     }
-    #section1{padding-top:5%;height:100%;}
-    #section2{padding-top:5%;height:100%;}
-    #section3{padding-top:5%;height:100%;}
+    #memories{height:40%;}
+    #fails{height:40%;}
+    #sons{height:40%;}
 
     .bgimg-1, .bgimg-2, .bgimg-3 {
       position: relative;
-      opacity: 0.5;
+      opacity: 0.6;
       background-attachment: fixed;
       background-position: center;
       background-repeat: no-repeat;
@@ -34,19 +35,20 @@
     }
 
     .bgimg-2 {
-      background-image: url("img/bg002.jpg");
-      min-height: 60%;
+      background-image: url("img/bg003.jpg");
+      min-height: 80%;
     }
 
     .bgimg-3 {
-      background-image: url("img/bg003.jpg");
-      min-height: 70%;
+      background-image: url("img/bg004.jpg");
+      min-height: 80%;
     }
 
     .caption {
       position: absolute;
       left: 0;
       top: 50%;
+      height: 100%;
       width: 100%;
       text-align: center;
       color: #000;
@@ -63,14 +65,31 @@
     h3 {
       letter-spacing: 5px;
       text-transform: uppercase;
-      font: 20px "Lato", sans-serif;
+      font: 24px "Lato", sans-serif;
+      color: #111;
+    }
+
+    p {
+      letter-spacing: 5px;
+      text-transform: uppercase;
+      font: 12px "Lato", sans-serif;
       color: #111;
     }
 
     .carousel-inner > .item > img {
         margin: 0 auto;
+        height: 100%;
     }
 
+    .carousel-inner,.carousel,.item,.container-fluid,.fill {
+        height:100%;
+        width:100%;
+    }
+
+    .container {
+        height:20%;
+        width:100%;
+    }
     </style>
 
     <!-- Bootstrap -->
@@ -95,14 +114,14 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Anniversary Website</a>
+          <a class="navbar-brand" href="#">Happy Anniversary</a>
         </div>
         <div>
           <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
-              <li><a href="#section1">Memories</a></li>
-              <li><a href="#section2">Videos</a></li>
-              <li><a href="#section3">MarryMe</a></li>
+              <li><a href="#memories">Memories</a></li>
+              <li><a href="#fails">Fails</a></li>
+              <li><a href="#sons">Sons</a></li>
             </ul>
           </div>
         </div>
@@ -116,49 +135,90 @@
     </div>
 
     <!--section1 with parallax-->
-    <div id="section1" class="container-fluid">
-      <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+    <div id="memories" class="container">
+      <div style="color: #777;background-color:white;text-align:center; padding: 60px 80px ;text-align: justify;">
         <h3 style="text-align:center;">We've been together for 4 years！</h3>
         <p style="text-align:center;">
-          第四個年頭發生了好多事情，先來回顧一下吧！
+          Lots of things happen in our 4th year, let's take a look
         </p>
       </div>
-      @yield('memories')
+    </div>
 
-      <div id="myCarousel" class="carousel slide"  data-ride="carousel">
+    <div class="container-fluid">
+      <div id="slide1" class="carousel slide"  data-ride="carousel">
         <!-- Indicators -->
         <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-          <li data-target="#myCarousel" data-slide-to="3"></li>
+          <li data-target="#slide1" data-slide-to="0" class="active"></li>
+          <li data-target="#slide1" data-slide-to="1"></li>
+          <li data-target="#slide1" data-slide-to="2"></li>
+          <li data-target="#slide1" data-slide-to="3"></li>
+          <li data-target="#slide1" data-slide-to="4"></li>
+          <li data-target="#slide1" data-slide-to="5"></li>
+          <li data-target="#slide1" data-slide-to="6"></li>
+          <li data-target="#slide1" data-slide-to="7"></li>
+          <li data-target="#slide1" data-slide-to="8"></li>
+          <li data-target="#slide1" data-slide-to="9"></li>
+          <li data-target="#slide1" data-slide-to="10"></li>
+          <li data-target="#slide1" data-slide-to="11"></li>
         </ol>
 
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
           <div class="item active">
-            <img src="img/img001.jpg">
+            <img src="img/mems/img001.jpg">
           </div>
 
           <div class="item">
-            <img src="img/img002.jpg">
+            <img src="img/mems/img002.jpg">
           </div>
 
           <div class="item">
-            <img src="img/img003.jpg">
+            <img src="img/mems/img003.jpg">
           </div>
 
           <div class="item">
-            <img src="img/img004.jpg">
+            <img src="img/mems/img004.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/mems/img005.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/mems/img006.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/mems/img006.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/mems/img007.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/mems/img008.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/mems/img009.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/mems/img010.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/mems/img011.jpg">
           </div>
         </div>
 
         <!-- Left and right controls -->
-        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <a class="left carousel-control" href="#slide1" role="button" data-slide="prev">
           <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
-        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <a class="right carousel-control" href="#slide1" role="button" data-slide="next">
           <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
@@ -167,9 +227,243 @@
       <!--add carousel here-->
     </div>
 
+    <div class="bgimg-2">
+      <div class="caption">
+        <span class="border">手牽手，一起走下去</span>
+      </div>
+    </div>
+
+    <div id="fails" class="container-fluid">
+      <div style="color: #777; background-color:white; text-align:center; padding: 60px 80px; text-align: justify; ">
+        <h3 style="text-align:center;">It's been a great time with you <3 </h3>
+        <p style="text-align:center;">
+          Enough touching, we should see those 'special' moments in our life!
+        </p>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div id="slide2" class="carousel slide"  data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#slide2" data-slide-to="0" class="active"></li>
+          <li data-target="#slide2" data-slide-to="1"></li>
+          <li data-target="#slide2" data-slide-to="2"></li>
+          <li data-target="#slide2" data-slide-to="3"></li>
+          <li data-target="#slide2" data-slide-to="4"></li>
+          <li data-target="#slide2" data-slide-to="5"></li>
+          <li data-target="#slide2" data-slide-to="6"></li>
+          <li data-target="#slide2" data-slide-to="7"></li>
+          <li data-target="#slide2" data-slide-to="8"></li>
+          <li data-target="#slide2" data-slide-to="9"></li>
+          <li data-target="#slide2" data-slide-to="10"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="img/fails/fail001.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/fails/fail002.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/fails/fail003.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/fails/fail004.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/fails/fail005.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/fails/fail006.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/fails/fail007.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/fails/fail008.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/fails/fail009.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/fails/fail010.jpg">
+          </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#slide2" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#slide2" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+      <!--add carousel here-->
+    </div>
+
+    <div class="bgimg-3">
+      <div class="caption">
+        <span class="border">小傢伙們，也很重要</span>
+      </div>
+    </div>
+
+    <div id="sons" class="container-fluid">
+      <div style="color: #777; background-color:white; text-align:center; padding: 60px 80px; text-align: justify; ">
+        <h3 style="text-align:center;"> Hope you won't be angry :P </h3>
+        <p style="text-align:center;">
+          They enriched our life so much :)
+        </p>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div id="slide3" class="carousel slide"  data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#slide3" data-slide-to="0" class="active"></li>
+          <li data-target="#slide3" data-slide-to="1"></li>
+          <li data-target="#slide3" data-slide-to="2"></li>
+          <li data-target="#slide3" data-slide-to="3"></li>
+          <li data-target="#slide3" data-slide-to="4"></li>
+          <li data-target="#slide3" data-slide-to="5"></li>
+          <li data-target="#slide3" data-slide-to="6"></li>
+          <li data-target="#slide3" data-slide-to="7"></li>
+          <li data-target="#slide3" data-slide-to="8"></li>
+          <li data-target="#slide3" data-slide-to="9"></li>
+          <li data-target="#slide3" data-slide-to="10"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="img/sons/son001.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/sons/son002.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/sons/son003.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/sons/son004.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/sons/son005.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/sons/son006.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/sons/son007.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/sons/son008.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/sons/son009.jpg">
+          </div>
+
+          <div class="item">
+            <img src="img/sons/son010.jpg">
+          </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#slide3" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#slide3" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+      <!--add carousel here-->
+    </div>
+
+
+    <script type="text/JavaScript">
+    $(document).ready(function(){
+      // Add scrollspy to <body>
+      $('body').scrollspy({target: ".navbar", offset: 50});
+
+      // Add smooth scrolling on all links inside the navbar
+      $("#myNavbar a").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+
+          // Store hash
+          var hash = this.hash;
+
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        }  // End if
+      });
+    });
+    </script>
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   </body>
+
+  <footer id="footerWrapper" class="footer3">
+  	<section id="mainFooter">
+  		<div class="container">
+  			<div class="row">
+  				<div class="col-sm-12">
+  					<div class="footerWidget text-center">
+              <div style="color: #777; background-color:white; text-align:center; padding: 60px 80px; text-align: justify; ">
+                <h1 style="font: 28px/1.5 Tahoma,Helvetica,Arial">謝謝妳四年來的照顧，未來還請多多指教</h1>
+              </div>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  	</section>
+  	<section id="footerRights">
+  		<div class="container">
+  			<div class="row">
+  				<div class="col-md-12">
+  					<p>Copyright © 2016 <a href="http://lcycoding.wordpress.com" target="blank">Lcycoding</a> / All rights reserved.</p>
+  				</div>
+  			</div>
+  		</div>
+  	</section>
+  </footer>
+
 </html>
